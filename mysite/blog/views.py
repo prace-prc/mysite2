62b6d4ac586475776abe48eb5114c55c7eccb339
Juwon Hwang <wndnjs719@gmail.com>
2023-07-26 11:59:22 +0900

    [기능 작성] 기본 urls, views 작성

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+# Create your views here.
+def post_list(request):
+    pass
+
+
+def post_detail(request, post_id):
+    pass
