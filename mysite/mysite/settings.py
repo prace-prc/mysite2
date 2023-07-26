f639620adaebaad2936937064049e7508bca9c14
Juwon Hwang <wndnjs719@gmail.com>
2023-07-26 09:25:05 +0900

    [기본 설정] DB를 postgresql로 설정

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # sqlite 설정
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '123456'
     }
 }
 
