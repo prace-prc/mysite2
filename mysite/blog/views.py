062159ae18339029e7348bb3aa11d11c993ab7b1
Juwon Hwang <wndnjs719@gmail.com>
2023-07-26 15:36:28 +0900

    [기능 작성] views.py와 list.html에 pagination 기능 추가

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,5 @@
+from django.core import paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.shortcuts import render
@@ -7,7 +9,23 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    page = request.GET.get('page')
+
+    paginator = Paginator(posts, 3)
+
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        page_obj = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        page_obj = paginator.page(page)
+
+    return render(request, 'blog/post/list.html',
+                  {'posts': posts,
+                   'page_obj': page_obj,
+                   'paginator': paginator})
 
 
 def post_detail(request, post_id):
