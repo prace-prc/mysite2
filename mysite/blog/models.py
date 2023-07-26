64a94eb83aab7c760b65e05ba83bff2157cefb33
Juwon Hwang <wndnjs719@gmail.com>
2023-07-26 10:18:06 +0900

    [기능 작성] Post 모델 작성

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', '게시되지 않음'
+        PUBLISHED = 'PB', '게시됨'
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        choices=Status.choices, max_length=2, default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
