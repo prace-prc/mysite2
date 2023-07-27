8cf67d9f9c696c4fc9c233524ce36fb5962d16a6
Juwon Hwang <wndnjs719@gmail.com>
2023-07-27 13:03:48 +0900

    [기능 작성] forms.py CommentModelForm 작성

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.utils import timezone
+
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     sender = forms.CharField(max_length=50)
     email = forms.EmailField()
     to = forms.EmailField()
+
+
+class CommentModelForm(forms.Form):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
