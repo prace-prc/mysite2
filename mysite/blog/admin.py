2e365c674392c9ae4871cd84d57e1faa0e93b0a4
Juwon Hwang <wndnjs719@gmail.com>
2023-07-27 11:36:22 +0900

    [기능 수정] commentadmin 필터 추가

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish', 'status']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'name', 'email', 'body', 'active']
+    list_filter = ['active', 'created', 'updated']
