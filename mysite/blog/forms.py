b1b43f1e2ee96ad54f9be47dd75f97958f3086d5
Juwon Hwang <wndnjs719@gmail.com>
2023-07-27 13:50:53 +0900

    [기능 작성] forms.py CommentModelForm 수정, (This field is required 라는 문구가 계속 나옴)

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -10,7 +10,13 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
 
 
-class CommentModelForm(forms.Form):
+class CommentModelForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentModelForm, self).__init__(*args, **kwargs)
+        self.fields['name'].required = False
+        self.fields['email'].required = False
+        self.fields['body'].required = False
